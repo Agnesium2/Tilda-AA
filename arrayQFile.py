5f139c851f592247c1d066968091324c574a9cde
Agnesium <amhultin@gmail.com>
2026-09-06 17:29:13 +0200

    tog bort två blank space

--- arrayQFile.py
+++ arrayQFile.py
@@ -6,13 +6,11 @@ class ArrayQ:
     def enqueue(self,item):
         self.__array.append(item)
 
-    
     def dequeue(self):
         item = self.__array.pop(0)
         return item
     
     __array = array
-
-
+    
     def __str__(self):
         return str(self.__array[0])

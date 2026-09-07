1b4a6196c0931431bb2862c29369a725c16aa5c7
Agnesium <amhultin@gmail.com>
2026-09-07 16:19:04 +0200

    Add files via upload

--- Labb3/Labb3.py
+++ Labb3/Labb3.py
@@ -1,9 +1,48 @@
-svenska = Bintree()             # Skapa ett trädobjekt
-svenska.put("gurka")		    # Sortera in "gurka" i trädet	
+from bintreeFile import Bintree
+'''
+def makeTree():
+    tree = Bintree()
+    data = input().strip()
+    while data != "#":
+        tree.put(data)
+        data = input().strip()
+    return tree
 
+def searches(tree):
+    findme = input().strip()
+    while findme != "#":
+        if findme in tree:
+            print(findme, "found")
+        else:
+            print(findme, "not found")
+        findme = input().strip()
 
-if "gurka" in svenska:          # Kolla om "gurka" finns i trädet
-                                # (Operatorn in anropar metoden __contains__ 
-                                # som du ska implementera i din Bintree-klass)
-    
-svenska.write()                 # Skriver alla trädobjektets ord i bokstavsordning
+def main():
+    tree = makeTree()
+    searches(tree)
+    tree.write()
+
+main()
+'''
+
+svenska = Bintree()
+with open("word3.txt", "r", encoding = "utf-8") as svenskfil:
+    for rad in svenskfil:
+        ordet = rad.strip()                # Ett trebokstavsord per rad
+        if ordet in svenska:
+            print(ordet, end = " ") 
+        else:
+            svenska.put(ordet)             # in i sökträdet
+print("\n")
+
+engelska = Bintree()
+with open("engelska.txt", "r") as engelskfil:
+    for rad in engelskfil:
+        ordrad = rad.strip().split(" ")               # Ett trebokstavsord per rad
+        for ord in ordrad:
+            if ord not in engelska:
+                engelska.put(ord)  
+                if ord in svenska:
+                    print(ord, end = " ")       # in i sökträdet
+engelska.write()
+print("\n")
--- Labb3/bintreeFile.py
+++ Labb3/bintreeFile.py
@@ -22,10 +22,34 @@ class Bintree:
         print("\n")
 
 def putta(p, newvalue):
+    if p == None:
+        return Node(newvalue)
+    else:
+        if newvalue < p.value:
+            p.left = putta(p.left, newvalue)
+        elif newvalue > p.value:
+            p.right = putta(p.right, newvalue)
+    return p
+
 # Funktion som gör själva jobbet att stoppa in en ny nod
 
-def finns(p,value):
+def finns(p, value):
+    if p:
+        if value == p.value:
+            return True
+        elif value < p.value:
+            return finns(p.left, value)
+        else:
+            return finns(p.right, value)
+    else:
+        return False
+    
 # Funktion som gör själva jobbet att söka efter ett värde
 
 def skriv(p):
+    if p:
+        skriv(p.left)
+        print(p.value)
+        skriv(p.right)
+
 # Funktion som gör själva jobbet att skriva ut trädet

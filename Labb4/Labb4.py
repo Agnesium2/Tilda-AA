6b91c7fb47155443ce077f2b7df3f1f5e412a4be
Agnesium <amhultin@gmail.com>
2026-09-15 13:25:35 +0200

    Update Labb4.py

--- Labb4/Labb4.py
+++ Labb4/Labb4.py
@@ -1,6 +1,15 @@
 from bintreeFile import Bintree
 from linkedQFile import LinkedQ
 
+def readfile(filename):
+    svenska = Bintree()
+    with open(filename, "r", encoding = "utf-8") as svenskfil:
+        for rad in svenskfil:
+            ordet = rad.strip()                # Ett trebokstavsord per rad
+            if ordet not in svenska:
+                svenska.put(ordet)             # in i sökträdet
+    return svenska
+
 def makechildren(q, svenska, gamla, ord, slutord):
     alfabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","å","ä","ö"]
     bokstav = list(ord)
@@ -9,30 +18,14 @@ def makechildren(q, svenska, gamla, ord, slutord):
         for i in range(29):
             bokstav[j] = alfabet[i]
             testord = "".join(bokstav)
-            if testord in svenska:
-                if testord not in gamla:
-                    q.enqueue(testord)
-                    gamla.put(testord)
-                    if testord == slutord:
-                        return True
+            if testord in svenska and testord not in gamla:
+                q.enqueue(testord)
+                gamla.put(testord)
+                if testord == slutord:
+                    return True
         bokstav[j] = current
 
-def main():
-    svenska = Bintree()
-    with open("word3.txt", "r", encoding = "utf-8") as svenskfil:
-        for rad in svenskfil:
-            ordet = rad.strip()                # Ett trebokstavsord per rad
-            if ordet not in svenska:
-                svenska.put(ordet)             # in i sökträdet
-
-    q = LinkedQ()
-    gamla = Bintree()
-
-    hittad = False
-
-    startord = input("Startord: ")
-    slutord = input("Slutord: ")
-
+def sök(startord, slutord, svenska, gamla, q):
     if startord in svenska:
         gamla.put(startord)
         q.enqueue(startord)
@@ -41,7 +34,20 @@ def main():
         ord = q.dequeue()
         hittad = makechildren(q, svenska, gamla, ord, slutord)
         if hittad == True:
-            break
+            return True
+
+def main():
+    svenska = readfile("word3.txt")
+
+    q = LinkedQ()
+    gamla = Bintree()
+
+    hittad = False
+    
+    startord = input("Startord: ")
+    slutord = input("Slutord: ")
+
+    hittad = sök(startord, slutord, svenska, gamla, q)
 
     if hittad == True:
         print("Det finns en väg till: ", slutord)
